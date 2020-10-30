d2ee6d491ace14c7450514d2e27709db4c1dd2ce
pstupka <patryk.stupka@gmail.com>
2020-10-30 19:51:16 +0100

    merge2

--- src/scripts/GameController.gd
+++ src/scripts/GameController.gd
@@ -1,8 +1,6 @@
 extends Node
 
-<<<<<<< HEAD
 enum ARCADE_TYPE {PUNG, ALLEYWAY, INVADOORS, HANDOIDS}
->>>>>>> 9691b93ca3e3bdd6c0a9f389de783de2a43407e5
 
 func _on_arcade_game_over(score, name):
 	print("Score ", score)
@@ -15,7 +13,6 @@ func load_arcade_control(game):
 	var arcade_to_load = ResourceLoader.load("res://src/levels/ArcadeControl.tscn").instance()
 	match game:
 		ARCADE_TYPE.PUNG:
-<<<<<<< HEAD
 			arcade_to_load = ResourceLoader.load("res://src/levels/ArcadeControl.tscn").instance()
 			arcade_to_load.load_game("res://src/levels/pung/PungGame.tscn", "res://assets/environment/arcade/arcade_pong.png") 
 			SceneChanger.transition_to(arcade_to_load)
@@ -31,5 +28,4 @@ func load_arcade_control(game):
 			arcade_to_load = ResourceLoader.load("res://src/levels/ArcadeControl.tscn").instance()
 			arcade_to_load.load_game("res://src/levels/handoids/handoidsgame.tscn", "res://assets/environment/arcade/arcade_pong.png") 
 			SceneChanger.transition_to(arcade_to_load)
->>>>>>> 9691b93ca3e3bdd6c0a9f389de783de2a43407e5
 	
